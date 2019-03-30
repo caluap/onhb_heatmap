70f69f3ec6441248541ed5cc8f7b685371790e55
Calua de Lacerda Pataca <calua.pataca@gmail.com>
2019-03-30 12:23:40 -0300

    sets random points for each team around each city — although there seems to be some coordinates out of whack

--- p5js/js/main.js
+++ p5js/js/main.js
@@ -1,34 +1,39 @@
 var canvas;
 let debug = true;
-let margin = 0.05;
-let n = 14000;
+let margin = 0.0;
 let points = [];
+
+let min_x, max_x, min_y, max_y;
 
 // NORTE (ponto mais setentrional):
 // Nascente do rio Ailã (extremo norte do estado de Roraima, fronteira com a Guiana)
 // - Latitude: +05° 16'19"
 // - Longitude: -60° 12'45"
-let min_y = "5° 16' 19\"";
+let north = "5° 16' 19\"";
 
 // SUL (ponto mais meridional):
 // Arroio Chuí (extremo sul do estado do Rio Grande do Sul, fronteira com o Uruguai)
 // - Latitude: -33° 45'07"
 // - Longitude: -53° 23'50"
-let max_y = "-33° 45' 07\"";
+let south = "-33° 45' 07\"";
 
 // LESTE (ponto mais oriental):
 // Ponta do Seixas (Cabo Branco-Paraíba)
 // - Latitude: -07° 09'18"
 // - Longitude: -34° 47'34"
-let max_x = "-34° 47' 34\"";
+let east = "-34° 47' 34\"";
 
 // OESTE (ponto mais ocidental):
 // Nascente do rio Moa (extremo oeste do estado do Acre, fronteira com o Peru)
 // - Latitude: -07° 32'09"
 // - Longitude: -73° 59'26"
-let min_x = "-73° 59' 26\"";
+let west = "-73° 59' 26\"";
 
-function preload() {}
+let data;
+
+function preload() {
+  data = loadJSON("data/processed_data.json");
+}
 
 function setup() {
   canvas = createCanvas(1080, 1080);
@@ -36,22 +41,31 @@ function setup() {
 
   let g = new GeoPoint();
 
-  min_y = g.deg2dec(min_y);
-  max_y = g.deg2dec(max_y);
+  min_lat = g.deg2dec(south);
+  max_lat = g.deg2dec(north);
 
-  min_x = g.deg2dec(min_x);
-  max_x = g.deg2dec(max_x);
+  min_long = g.deg2dec(east);
+  max_long = g.deg2dec(west);
 
-  let x0 = width * (1 - margin);
-  let x1 = width * margin;
-  let y0 = height * (1 - margin);
-  let y1 = height * margin;
+  min_x = width * (1 - margin);
+  max_x = width * margin;
+  min_y = height * (1 - margin);
+  max_y = height * margin;
 
-  for (let i = 0; i < n; i++) {
-    let x = map(random(), 0, 1, x0, x1);
-    let y = map(random(), 0, 1, y0, y1);
-    points.push(createVector(x, y));
-  }
+  let radius_adj = 0.2;
+
+  data.data.forEach(e => {
+    let r = e.qtd * radius_adj;
+    for (let i = 0; i < e.qtd; i++) {
+      let rand_r = random() * r;
+      let rand_a = random() * TWO_PI;
+      let _x = sin(rand_a) * rand_r;
+      let _y = cos(rand_a) * rand_r;
+      let cx = map(e.long, min_long, max_long, min_x, max_x);
+      let cy = map(e.lat, min_lat, max_lat, min_y, max_y);
+      points.push(createVector(cx + _x, cy + _y));
+    }
+  });
 }
 
 function draw() {
